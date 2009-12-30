a9ac8ef4cd54c74ebaeb54e11a9bf96919706c24
Fajran Iman Rusadi <fajran@gmail.com>
2009-12-30 18:02:42 +0100

    Import urls

--- src/gerobak/urls.py
+++ src/gerobak/urls.py
@@ -4,9 +4,16 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
+import gerobak.apps.profile
+import gerobak.apps.queue
+
 urlpatterns = patterns('',
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', 
         {'document_root': settings.MEDIA_ROOT}),
 
     (r'^admin/', include(admin.site.urls)),
+
+    (r'^p/', include(gerobak.apps.profile.urls)),
+    (r'^q/', include(gerobak.apps.queue.urls)),
+    (r'^$', 'gerobak.views.index'),
 )
