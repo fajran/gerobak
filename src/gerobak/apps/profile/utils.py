e22384663dcbbed462d08abf87dd659e6d8746a7
Fajran Iman Rusadi <fajran@gmail.com>
2010-01-09 08:53:46 +0100

    apt-get install result parser

---
+++ src/gerobak/apps/profile/utils.py
@@ -0,0 +1,36 @@
+import re
+
+_pkg_url_re = re.compile(u"^'([^']+)' ([^ ]+) (\d+) (([^:]+:)?.+)")
+def _parse_pkg_url(line):
+    return _pkg_url_re.match(line).groups()
+
+def parse_apt_install(out):
+    res = []
+
+    extra = []
+    suggested = []
+    recommended = []
+    install = []
+    packages = []
+    newest = []
+
+    out2 = out.replace("\n  ", "  ")
+    for line in out2.splitlines():
+        if line.endswith('is already the newest version.'):
+            newest.append(line.split(' ', 1)[0])
+        elif line.startswith('The following extra packages will be installed:'):
+            extra = line.split(':', 1)[1].strip().split()
+        elif line.startswith('Suggested packages:'):
+            suggested = line.split(':', 1)[1].strip().split()
+        elif line.startswith('Recommended packages:'):
+            recommended = line.split(':', 1)[1].strip().split()
+        elif line.startswith('The following NEW packages will be installed:'):
+            install = line.split(':', 1)[1].strip().split()
+        elif line.startswith("'"):
+            packages.append(_parse_pkg_url(line))
+
+    return extra, suggested, recommended, install, packages, newest
+
+if __name__ == '__main__':
+    print parse_apt_install(open('/tmp/apt-install.txt').read())
+
