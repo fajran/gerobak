3f6eca8b5eb353fff05678c40daa4c1b83f556a1
Fajran Iman Rusadi <fajran@gmail.com>
2010-01-10 03:46:07 +0100

    Exact URL match

--- src/gerobak/apps/profile/urls.py
+++ src/gerobak/apps/profile/urls.py
@@ -4,12 +4,13 @@ from django.conf import settings
 from gerobak.apps.profile import views
 
 urlpatterns = patterns('',
-    (r'^(?P<pid>\d+)/install/', views.install),
-    (r'^(?P<pid>\d+)/search/', views.search),
-    (r'^(?P<pid>\d+)/update/', views.update),
-    (r'^(?P<pid>\d+)/sources/', views.sources),
-    (r'^(?P<pid>\d+)/status/', views.status),
+    (r'^(?P<pid>\d+)/install/$', views.install),
+    (r'^(?P<pid>\d+)/search/$', views.search),
+    (r'^(?P<pid>\d+)/update/$', views.update),
+    (r'^(?P<pid>\d+)/sources/$', views.sources),
+    (r'^(?P<pid>\d+)/status/$', views.status),
+    (r'^(?P<pid>\d+)/show/(?P<pkg>[a-z0-9.-]+)/$', views.info),
     #(r'^(?P<pid>\d+)/sources/', include(gerobak.apps.source.urls)),
-    (r'^(?P<pid>\d+)/', views.show),
+    (r'^(?P<pid>\d+)/$', views.show),
     (r'^$', views.index),
 )
