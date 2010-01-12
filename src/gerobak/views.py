6636ffb33ca83d29f2d574884db9d5ef22e6b437
Fajran Iman Rusadi <fajran@gmail.com>
2010-01-12 01:58:17 +0100

    gerobak's views

---
+++ src/gerobak/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+
+def index(request):
+    if request.user.is_authenticated():
+        return redirect('gerobak.apps.profile.views.index')
+    return render_to_response('index.html',
+                              context_instance=RequestContext(request))
