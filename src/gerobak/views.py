e9038f8530d59ca66bede985acbdda559dc7edff
Fajran Iman Rusadi <fajran@gmail.com>
2010-01-13 21:04:43 +0100

    Redirect anonymous users to the how to page when they open the main page.

--- src/gerobak/views.py
+++ src/gerobak/views.py
@@ -4,5 +4,7 @@ from django.template import RequestContext
 def index(request):
     if request.user.is_authenticated():
         return redirect('gerobak.apps.profile.views.index')
-    return render_to_response('index.html',
-                              context_instance=RequestContext(request))
+    return redirect('/doc/howto/')
+    #return render_to_response('index.html',
+    #                          context_instance=RequestContext(request))
+
